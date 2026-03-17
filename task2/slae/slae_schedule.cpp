8747b15d9ab9382cfd828a5d7e6038be400149dc
LijnxServer <lynx20030311@gmail.com>
2026-03-17 20:17:39 +0000

    fix and ran slae section test

--- task2/slae/slae_schedule.cpp
+++ task2/slae/slae_schedule.cpp
@@ -5,6 +5,17 @@
 #include <omp.h>
 
 
+using Slae = void (*)(
+    const std::vector<double>&, 
+    std::vector<double>&, 
+    const std::vector<double>&, 
+    int, 
+    double, 
+    double,
+    int
+);
+
+
 void slae_static(
     const std::vector<double>& a, 
     std::vector<double>& x, 
@@ -137,7 +148,7 @@ void slae_dynamic(
 }
 
 
-void slae_guide(
+void slae_guided(
     const std::vector<double>& a, 
     std::vector<double>& x, 
     const std::vector<double>& b, 
@@ -155,7 +166,7 @@ void slae_guide(
     #pragma omp parallel
     {
 
-        #pragma omp for schedule(guide, k) reduction(+:b_norm)
+        #pragma omp for schedule(guided, k) reduction(+:b_norm)
         for (int i = 0; i < n; ++i) {
             b_norm += b[i] * b[i];
         }
@@ -173,7 +184,7 @@ void slae_guide(
                 norm_sq = 0.0;
             }
 
-            #pragma omp for schedule(guide, k) reduction(+:norm_sq)
+            #pragma omp for schedule(guided, k) reduction(+:norm_sq)
             for (int i = 0; i < n; ++i) {
 
                 const double* row = &a[i * n];
@@ -187,7 +198,7 @@ void slae_guide(
                 norm_sq += ri * ri;
             }
 
-            #pragma omp for schedule(guide, k)
+            #pragma omp for schedule(guided, k)
             for (int i = 0; i < n; ++i) {
                 x[i] = x_new[i];
             }
@@ -203,7 +214,7 @@ void slae_guide(
 }
 
 
-double run_static(int n, int k) {
+double run_parallel(int n, int k, Slae slae) {
 
     std::vector<double> a(n * n);
     std::vector<double> x(n);
@@ -211,7 +222,7 @@ double run_static(int n, int k) {
     double t = 1.0e-5;
     double e = 1.0e-5;
 
-    #pragma omp parallel for schedule(static, k)
+    #pragma omp parallel for schedule(static)
     for (int i = 0; i < n; ++i) {
         double* row = &a[i * n];
         for (int j = 0; j < n; ++j) {
@@ -224,7 +235,7 @@ double run_static(int n, int k) {
     
     const auto start{std::chrono::steady_clock::now()};
 
-    slae_static(a, x, b, n, t, e, k);
+    slae(a, x, b, n, t, e, k);
 
     const auto end{std::chrono::steady_clock::now()};
     const std::chrono::duration<double> elapsed_seconds(end - start);
@@ -232,74 +243,13 @@ double run_static(int n, int k) {
     return elapsed_seconds.count();
 }
 
-
-double run_dynamic(int n, int k) {
-
-    std::vector<double> a(n * n);
-    std::vector<double> x(n);
-    std::vector<double> b(n);
-    double t = 1.0e-5;
-    double e = 1.0e-5;
-
-    #pragma omp parallel for schedule(dynamic, k)
-    for (int i = 0; i < n; ++i) {
-        double* row = &a[i * n];
-        for (int j = 0; j < n; ++j) {
-            row[j] = 1.0;
-        }
-        row[i] = 2.0;
-        b[i] = n + 1.0;
-        x[i] = 0.0;
-    }
-    
-    const auto start{std::chrono::steady_clock::now()};
-
-    slae_dynamic(a, x, b, n, t, e, k);
-
-    const auto end{std::chrono::steady_clock::now()};
-    const std::chrono::duration<double> elapsed_seconds(end - start);
-
-    return elapsed_seconds.count();
-}
-
-
-double run_guide(int n, int k) {
-
-    std::vector<double> a(n * n);
-    std::vector<double> x(n);
-    std::vector<double> b(n);
-    double t = 1.0e-5;
-    double e = 1.0e-5;
-
-    #pragma omp parallel for schedule(guide, k)
-    for (int i = 0; i < n; ++i) {
-        double* row = &a[i * n];
-        for (int j = 0; j < n; ++j) {
-            row[j] = 1.0;
-        }
-        row[i] = 2.0;
-        b[i] = n + 1.0;
-        x[i] = 0.0;
-    }
-    
-    const auto start{std::chrono::steady_clock::now()};
-
-    slae_guide(a, x, b, n, t, e, k);
-
-    const auto end{std::chrono::steady_clock::now()};
-    const std::chrono::duration<double> elapsed_seconds(end - start);
-
-    return elapsed_seconds.count();
-}
-
-using Run_func = double (*) (int, int);
 
 void benchmark(
     int data_size, 
     int tests_num, 
     const std::vector<int>& test_threads, 
     std::vector<int>& test_chunks,
-    Run_func run,
+    Slae slae,
     std::string output_file_name
 ) {
 
@@ -314,7 +264,7 @@ void benchmark(
         for (int chunk_size : test_chunks) {
 
             for (int t = 0; t < tests_num; ++t) {
-                double time = run(data_size, chunk_size);
+                double time = run_parallel(data_size, chunk_size, slae);
                 fout << nthreads   << ", "
                      << chunk_size << ", "
                      << time       << std::endl;
@@ -331,11 +281,11 @@ int main(int argc, char** argv) {
     const int data_size = 10000;
     const int tests_num = 100;
     std::vector<int> test_threads{1,2,4,7,8,16,20,40};
-    std::vector<int> test_chunks{1,10,100};
+    std::vector<int> test_chunks{1,10,100,1000};
 
-    benchmark(data_size, tests_num, test_threads, test_chunks, run_static, "slae_static.csv");
-    benchmark(data_size, tests_num, test_threads, test_chunks, run_dynamic, "slae_dynamic.csv");
-    benchmark(data_size, tests_num, test_threads, test_chunks, run_guide, "slae_guide.csv");
+    benchmark(data_size, tests_num, test_threads, test_chunks, slae_static, "slae_static.csv");
+    benchmark(data_size, tests_num, test_threads, test_chunks, slae_dynamic, "slae_dynamic.csv");
+    benchmark(data_size, tests_num, test_threads, test_chunks, slae_guided, "slae_guided.csv");
 
     return 0;
 }
